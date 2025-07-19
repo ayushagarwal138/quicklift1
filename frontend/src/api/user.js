8ccc8d823bdb337ebf45c218de37c5895a6446ba
Ayush Agarwal <agarwalayush318@gmail.com>
2025-07-19 09:08:15 +0530

    fix: use correct /api/users/profile endpoints in user API calls

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,17 +2,17 @@ import api from './axios';
 
 export const userAPI = {
   getProfile: async () => {
-    const response = await api.get('/users/profile');
+    const response = await api.get('/api/users/profile');
     return response.data;
   },
 
   updateProfile: async (profileData) => {
-    const response = await api.put('/users/profile', profileData);
+    const response = await api.put('/api/users/profile', profileData);
     return response.data;
   },
   
   updateProfilePicture: async (profilePictureUrl) => {
-    const response = await api.put('/users/profile/picture', { profilePictureUrl });
+    const response = await api.put('/api/users/profile/picture', { profilePictureUrl });
     return response.data;
   },
 }; 
