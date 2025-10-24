7bf62f060f9450991bf9bdffbfa778218ab1a958
Ayush Agarwal <agarwalayush318@gmail.com>
2025-10-24 12:31:42 +0530

    Add explicit component scanning and test controller

--- backend/src/main/java/com/quicklift/backend/RideshareBackendApplication.java
+++ backend/src/main/java/com/quicklift/backend/RideshareBackendApplication.java
@@ -2,8 +2,10 @@ package com.quicklift.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.quicklift.backend")
 public class RideshareBackendApplication {
 
 	public static void main(String[] args) {
